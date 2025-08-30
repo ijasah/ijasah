cd7743142f6ca783b36acec4d84489c755342911
IJAS A H <41640210+ijasah@users.noreply.github.com>
2025-08-30 07:51:44 +0000

    quick change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/subtle-folio-toggle/' : '/',
+  base: mode === 'production' ? '/subtle-folio-toggle/' : '/', // Set base for GitHub Pages
   server: {
     host: "::",
     port: 8080,
