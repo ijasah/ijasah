42264a81fbfab7ff85c4e29bfbe2cfb5c37efb93
IJAS A H <41640210+ijasah@users.noreply.github.com>
2025-08-30 07:59:43 +0000

    quick change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -14,7 +14,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
+  <HashRouter>
         <AnimatePresence mode="wait">
           <Routes>
             <Route path="/" element={<Index />} />
@@ -25,7 +25,7 @@ const App = () => (
         </AnimatePresence>
         <Sonner />
         <Toaster />
-      </BrowserRouter>
+  </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
